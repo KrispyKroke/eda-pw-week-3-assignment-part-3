4111ee94c6b1f781b2b88ff016eaffbc77d348ab
Jared Kroke <jared.kroke@gmail.com>
2021-11-23 14:16:30 -0600

    Done with the assignment for week 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -42,11 +42,33 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for (change of supplyChanges) {
+  if (change > 0) {
+    console.log("Added " + change + " parts.");
+  } else if (change === 0) {
+    console.log("No Change.");
+  } else {
+    console.log("Removed " + Math.abs(change) + " parts.");
+  }
+}
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+let index = 0;
+while (index < supplyChanges.length) {
+  if (supplyChanges[index] > 0) {
+    console.log("Added " + supplyChanges[index] + " parts.");
+  } else if (supplyChanges[index] === 0) {
+    console.log("No Change.");
+  } else {
+    console.log("Removed " + Math.abs(supplyChanges[index]) + " parts.");
+  }
+  index++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
+let totalSupplies = 0;
+for (number of supplyChanges) {
+  totalSupplies += number;
+}
+console.log('9. Total supplies available is:', totalSupplies);
